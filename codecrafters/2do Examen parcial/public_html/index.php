9e1dabb860d4da9cc5b2c4193e1301aef90e66d9
surebria <142252689+surebria@users.noreply.github.com>
2023-10-28 14:16:29 -0600

    Add files via upload

--- codecrafters/2do Examen parcial/public_html/index.php
+++ codecrafters/2do Examen parcial/public_html/index.php
@@ -10,27 +10,8 @@
 </head>
 <body>
     <?php require 'header.html'; ?>
-    <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
     
+    <?php require 'vistas/inicio.html'; ?>
+    <?php require'footer.html'; ?>
 </body>
 </html>
